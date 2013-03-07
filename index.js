3c84edfba6ab6505b64da79f49ea98cad573d377
beastmode <brendan@wayla.com>
2013-03-07 00:55:56 -0500

    oh fuck forgot childcontroller.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.Model = require('./lib/Model');
 exports.ChildModel = require('./lib/ChildModel');
 exports.Controller = require('./lib/Controller');
+exports.ChildController = require('./lib/ChildController');
