0224aa15f32dd2eafe58b4504b43fb449802085d
brendanobrienesq <brendanobrienesq@gmail.com>
2013-10-23 14:05:49 -0400

    Add error object to Model & Controller.

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
 exports.Model = require('./lib/Model');
-exports.ChildModel = require('./lib/ChildModel');
 exports.Controller = require('./lib/Controller');
-exports.ChildController = require('./lib/ChildController');
